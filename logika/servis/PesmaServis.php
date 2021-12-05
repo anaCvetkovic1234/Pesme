82e24f42b6f076d612e3b4af3cb8d19108bc2ab3
anaCvetkovic1234 <anacvetkovic9999@gmail.com>
2021-12-05 23:43:27 +0100

    Kreiran servis za rad sa pesmama

---
+++ logika/servis/PesmaServis.php
@@ -0,0 +1,55 @@
+<?php
+
+class PesmaServis{
+    private $broker;
+    
+    public function __construct($b){
+        $this->broker=$b;
+    }
+
+    public function vratiSve(){
+        $pesme= $this->broker->ucitajKOlekciju("select p.*, z.naziv as 'zanr_naziv',pe.ime as 'pevac_ime',pe.prezime as 'pevac_prezime' from pesma p inner join zanr z on (z.id=p.zanr) inner join pevac pe on(pe.id=p.pevac)");
+        $res=[];
+        foreach($pesme as $pesma){
+            $res[]=$this->toDto($pesma);
+        }
+        return $res;
+    }
+    public function vratiJednu($id){
+        $pesma= $this->broker->ucitajJedan("select p.*, z.naziv as 'zanr_naziv',pe.ime as 'pevac_ime',pe.prezime as 'pevac_prezime' from pesma p inner join zanr z on (z.id=p.zanr) inner join pevac pe on(pe.id=p.pevac) where p.id=".$id);
+        return $this->toDto($pesma);
+    }
+    public function kreiraj($pesmaDto){
+        $this->broker->upisi("insert into pesma(naziv,trajanje,link,zanr,pevac) values('".$pesmaDto['naziv'].
+                                "',".$pesmaDto['trajanje'].",'".$pesmaDto['link']."',".$pesmaDto['zanr'].",".$pesmaDto['pevac'].")");
+        $id=  $this->broker->getLastId();
+        return $this->toDto($this->vratiJednu($id));
+    }
+    public function izmeni($id,$pesmaDto){
+        $this->broker->upisi("update pesma set naziv='".$pesmaDto['naziv']."', trajanje=".$pesmaDto['trajanje'].
+                                ", link='".$pesmaDto['link']."' , zanr=".$pesmaDto['zanr']." , pevac=".$pesmaDto['pevac']." where id=".$id);
+        return $this->toDto($this->vratiJednu($id));
+    }
+    public function obrisi($id){
+        $this->broker->upisi("delete from pesma where id=".$id);
+    }
+    private function toDto($pesma){
+        return [
+            "id"=>$pesma->id,
+            "naziv"=>$pesma->naziv,
+            "trajanje"=>$pesma->trajanje,
+            "link"=>$pesma->link,
+            "zanr"=>[
+                "id"=>$pesma->zanr,
+                "naziv"=>$pesma->zanr_naziv
+            ],
+            "pevac"=>[
+                "id"=>$pesma->pevac,
+                "ime"=>$pesma->pevac_ime,
+                "prezime"=>$pesma->pevac_prezime
+            ]
+        ];
+    }
+}
+
+?>
--- logika/servis/PisacServis.php
+++ logika/servis/PisacServis.php
@@ -1,6 +1,6 @@
 <?php
 
-class ZanrServis{
+class PisacServis{
     private $broker;
     
     public function __construct($b){
@@ -8,20 +8,23 @@ class ZanrServis{
     }
 
     public function vratiSve(){
-        return $this->broker->ucitajKOlekciju("select * from pisac");
+        return $this->broker->ucitajKOlekciju("select ime, prezime, godina_rodjenja as 'godinaRodjenja' from pisac");
     }
     public function kreiraj($pisacDto){
         $this->broker->upisi("insert into pisac(ime,prezime,godina_rodjenja) values ('".
                                 $pisacDto['ime']."',''".$pisacDto['prezime'].",".$pisacDto['godinaRodjenja'].")");
         $id=  $this->broker->getLastId();
-        return   $this->broker->ucitajJedan("select * from pisac where id=".$id);
+        return $this->vratiJedan($id);
     }
 
     public function izmeni($id,$pisacDto){
         $this->broker->upisi("update pisac set ime='".$pisacDto['ime'].
                                 "', prezime='".$pisacDto['prezime']."', godina_rodjenja=".
                                 $pisacDto['godinaRodjenja']." where id=".$id);
-        return   $this->broker->ucitajJedan("select * from pisac where id=".$id);
+        return $this->vratiJedan($id);
+    }
+    public function vratiJedan($id){
+        $this->broker->ucitajJedan("select ime, prezime, godina_rodjenja as 'godinaRodjenja' from pisac where id=".$id);
     }
     public function obrisi($id){
         $this->broker->upisi("delete from pisac where id=".$id);
